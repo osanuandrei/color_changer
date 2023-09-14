3927237f3675a142efb64ef573ddb8dc5198d63b
Emma Paris <osanu.andrei@yahoo.com>
2023-09-14 13:20:57 +0300

    full app

--- c-changer/src/App.js
+++ c-changer/src/App.js
@@ -1,7 +1,25 @@
 import './index.css';
+import Square from './Square';
+import Input from './Input';
+import { useState } from 'react';
 function App() {
+  const [colorValue, setColorValue] = useState('')
+  const [hexValue, setHexValue] = useState('')
+  const [isDarkText, setIsDarkText] = useState(true)
+
   return (
     <div className="App">
+      <Square 
+      hexValue = {hexValue}
+      colorValue={colorValue}
+      isDarkText={isDarkText}/>
+      <Input
+      isDarkText={isDarkText}
+      setIsDarkText={setIsDarkText}
+      setHexValue={setHexValue}
+      colorValue={colorValue}
+      setColorValue={setColorValue}
+      />
       
     </div>
   );
